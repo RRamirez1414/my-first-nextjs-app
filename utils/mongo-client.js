cb939a40603ae9457c7f8161766cf151e86eb11b
Ramon Ramirez <ramirez.ramon1414@gmail.com>
2022-09-02 11:54:40 -0600

    adjusted mongoclient function

--- utils/mongo-client.js
+++ utils/mongo-client.js
@@ -6,7 +6,9 @@ import { MongoClient } from 'mongodb'
  */
 async function getMongoClientCollection(collection) {
   const client = await MongoClient.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_SECRET}@cluster0.bhpyf9f.mongodb.net/meetups?retryWrites=true&w=majority`
+    `mongodb+srv://${'' + process.env.MONGODB_USERNAME}:${
+      '' + process.env.MONGODB_SECRET
+    }@cluster0.bhpyf9f.mongodb.net/meetups?retryWrites=true&w=majority`
   )
   const db = client.db()
 
